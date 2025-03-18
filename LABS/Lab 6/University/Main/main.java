43298609ed93dd2acd88aa13f87adc3ad5c58c17
Ahmad Ali Khan <aakthecool@gmail.com>
2025-03-18 14:27:35 +0500

    Create main.java

---
+++ LABS/Lab 6/University/Main/main.java
@@ -0,0 +1,22 @@
+package University.Main;
+
+import University.Employee.*;
+import University.SystemUnit.*;
+import University.Department.*;
+import University.Lab.*;
+
+public class Main{
+
+	public static void main(String args[]){
+	//	public Department(String name, Employee hod, String labIncharge, Lab labs[]){
+	//				public Lab(String name, Employee labStaff, SystemUnit systemUnits[], boolean hasMultimedia){
+		
+
+	//public SystemUnit(String cpuName, String lcdModel, double ramSize, double diskSize)
+
+
+
+
+		Department department = new Department("CS Department", new Employee("Ali", "HOD"), "Babar", new Lab("Ahmad", new Employee("Abdul", "Teacher"), new SystemUnit("Intel", "Samsung 2x", 16, 200), True);
+	}
+}
